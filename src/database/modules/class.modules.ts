a440b55382512b6702fc00bf2ba24329ddc22bae
vasudigivalet <vasudev.kanungo@digivalet.com>
2023-02-24 19:46:04 +0530

    initial commit

--- src/database/modules/class.modules.ts
+++ src/database/modules/class.modules.ts
@@ -1,29 +1,28 @@
-import { logger } from "../../utils/logger";
 import { ObjectId } from "mongodb";
+import { logger } from "../../utils/logger";
 import { classCollection } from "../model/class.model";
 
 export class Class {
   //create class
   createclass = (parents: any, args: any) => {
     try {
-      logger.info("A Class is created")
+      logger.info('A Class is created',args.input);
       const classcollection = new classCollection(args.input);
-      return classcollection.save();    
+      return classcollection.save();
     } catch (err) {
-      
       console.warn('something went wrong', err);
-      logger.error("something bad happend",err)
+      logger.error('something bad happend', err);
     }
   };
 
   // display class
   displayClass = async () => {
     try {
-      logger.info("Class Details displayed")
+      logger.info('Class Details displayed');
       return await classCollection.find();
     } catch (err) {
       console.warn('Something went Wrong', err);
-      logger.error('Something went Wrong',err)
+      logger.error('Something went Wrong', err);
     }
   };
   // displayClass = () => {
@@ -33,10 +32,10 @@ export class Class {
   //Display class by ID
   displayClassByID = async (parents: any, args: any) => {
     try {
-      logger.info("Successfully class of id:", args.id)
+      logger.info('Successfully class of id:', args._id);
       return await classCollection.findById(new ObjectId(args.id));
     } catch (err) {
-      logger.error("something went wrong",err )
+      logger.error('something went wrong', err);
       console.warn('Something went Wrong', err);
     }
   };
@@ -44,25 +43,23 @@ export class Class {
   //Update class by ID
   updateClass = async (parents: any, args: any) => {
     try {
-      logger.info("Successfully Update data from class")
-      return await classCollection.findByIdAndUpdate(args.id, args.input, {
+      logger.info('Successfully Update data from class',args._id);
+      return await classCollection.findByIdAndUpdate(args._id, args.input, {
         new: true,
       });
     } catch (err) {
-      logger.error("something went wrong",err )
+      logger.error('something went wrong', err);
       console.warn('Something went Wrong', err);
-      
     }
   };
 
   //Delete class by ID
   deleteClass = async (parents: any, args: any) => {
     try {
-      logger.info("Successfully Deleted data from class")
-      return await classCollection.findByIdAndRemove(args.id);
+      logger.info('Successfully Deleted data from class',args._id);
+      return await classCollection.findByIdAndRemove(args._id);
     } catch (err) {
-      logger.error("something went wrong",err )
-      console.warn('Something went Wrong', err);
+      logger.error('something went wrong', err);
     }
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import http from "http";
+import path from "path";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { resolvers } from "./graphQl/resolvers/index";
 import { typeDefs } from "./graphQl/schema";
+
 const { graphqlUploadExpress } = require('graphql-upload');
 
 const port = 4000;
@@ -33,7 +35,8 @@ const executeMain = async () => {
   });
 
   await server.start();
-
+  const staticpath = path.join(__dirname, './uploads');
+  app.use(express.static(staticpath));
   app.use(
     '/',
     cors<cors.CorsRequest>(),
@@ -43,11 +46,16 @@ const executeMain = async () => {
       context: async ({ req }) => ({ token: req.headers.token }),
     }),
   );
-
+  app.use(
+    bodyParser.urlencoded({
+      limit: '50mb',
+      extended: true,
+    }),
+  );
   await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
   console.log(`🚀 Server ready at http://localhost:${port}/`);
 };
 
-executeMain().catch((error:Error) => {
+executeMain().catch((error: Error) => {
   console.log(error, 'error');
 });
